93dccdcf663e56331f9f401c49a0891e3eb503ec
Jailton Júnior <jailton.junior94@outlook.com>
2020-02-10 20:27:12 -0300

    conclusão da criação do component header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
-  return <div> _app</div>;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Header />
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
