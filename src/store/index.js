272bac395ee5abcb0357c2f1d12ec7b0f06bd164
Jailton Júnior <jailton.junior94@outlook.com>
2020-02-11 14:10:40 -0300

    Conclusão do módulo: Arquitetura Flux

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+const sagaMiddlewere = createSagaMiddleware({ sagaMonitor });
 
 const enhancer =
-  process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+  process.env.NODE_ENV === 'development'
+    ? compose(console.tron.createEnhancer(), applyMiddleware(sagaMiddlewere))
+    : null;
 
 const store = createStore(rootReducer, enhancer);
+sagaMiddlewere.run(rootSaga);
 
 export default store;
