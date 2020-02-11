0c22d733712b8148a540ff9af43fa0de31fa9697
Jailton Júnior <jailton.junior94@outlook.com>
2020-02-10 21:56:39 -0300

    Conclusão da Estrutura da aplicação

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 'off',
-    'react/static-property-placement': 'off'
+    'react/static-property-placement': 'off',
+    'jsx-a11y/control-has-associated-label': 'off'
   },
 };
